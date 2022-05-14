2b67e0a1497e880d3858cccca748e007f829c1ae
Igor Masevič <104928045+EGO1ST77@users.noreply.github.com>
2022-05-14 23:52:25 +0300

    2022-05-14

--- 13/13.4.1 Тема урока_функции с возвратом значения.py
+++ 13/13.4.1 Тема урока_функции с возвратом значения.py
@@ -27,3 +27,39 @@ grade = int(input("Введите баш бал по 100-бальной сист
 print(f' Ваш бал = {convret_grade(grade)}')
 
 
+# Длина гипотенузы прямоугольного треугольника
+
+def compute_hypotenuse(a, b):
+    c = (a ** 2 + b ** 2) ** 0.5
+    return c
+
+
+def get_distance(x1, y1, x2, y2):
+    return compute_hypotenuse(x1 - x2, y1 - y2)
+
+
+print(f'Длина гипотенузы сторон A и B = {compute_hypotenuse(5, 7)}')
+print(f'Расстояние гипотинузы по точкам Х1, Х2, Y1, Y2 = {get_distance(1, 2, 3, 4)}')
+
+
+# Функция, которая возвращает назад сумму цифр числа
+
+def sum_digits(n):
+    result = 0
+    while n > 0:
+        result += n % 10
+        n //= 10
+    return result
+
+
+n = int(input('Введи число : '))
+print(f'Сумма чисел числа {n} равна {sum_digits(n)}')
+
+
+# функция возвращает среднее значение элементов списка
+
+def compute_average(numbers):
+    return sum(numbers) / len(numbers)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(compute_average(numbers))
